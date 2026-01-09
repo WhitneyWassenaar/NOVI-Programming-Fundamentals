990a5cd8782b5f2cba933b64cd3b82c5d89d7e53
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-01-09 18:12:09 +0100

    Chapter 7: Exercises

---
+++ Lesson-04/01-ShoppingList.py
@@ -0,0 +1,12 @@
+# Exercise 1
+# Write a shopping list with at least 5 items that you currently need from the supermarket.
+# Print the first and the third item from the list.
+
+shoppinglist = ["Bread", "Cheese", "Beef", "Eggs", "Chocolate", "Milk"]
+
+print(shoppinglist[0], shoppinglist[2])
+
+# Exercise 2
+# Print the middle items from the list
+
+print(shoppinglist[1:5])
