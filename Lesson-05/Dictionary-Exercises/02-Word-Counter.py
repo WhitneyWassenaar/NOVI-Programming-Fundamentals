10893f6b10bab7e7a648ee1697467a732bab1a84
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-01-12 17:41:20 +0100

    Add alternative solution

--- Lesson-05/Dictionary-Exercises/02-Word-Counter.py
+++ Lesson-05/Dictionary-Exercises/02-Word-Counter.py
@@ -14,10 +14,33 @@ for word in sentence:
 print(f'{count} woorden')
 
 # ---Word Counter v2---
-sentence = "I want to become a web developer!"
-sentence = sentence.split()
+sentence_1 = "I want to become a web developer!"
+sentence_1 = sentence_1.split()
 
-print(f'{len(sentence)} woorden')
+print(f'{len(sentence_1)} woorden')
+
+# ---Word Counter v3
+# Wrong, it only counts unique words
+sentence_2 = "I want to become a web developer!"
+words = {}
+sentence_2_s = sentence_2.split()
+
+for item in sentence_2_s:
+    words[item] = 1
+
+print(sum(words.values()))
+
+# ---Word Counter v4---
+sentence_3 = "I want to become a web developer!"
+
+words = {} # Create an empty dictionary to add words
+counter = 0 # Counts how many words are in the sentence
+
+for word in sentence_3.split(): # For every item in the list 'sentence_3'
+    words[counter] = word # Add a counter and the word from the list
+    counter += 1
+
+print(len(words)) # Length of dictionary == amount of words
 
 # ---Bonus---
 user_input = input('Write a sentence: ').split()
