46c0cbf362b8a9caa0d9e2025dc1cf790211e94b
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-01-23 09:23:32 +0100

    Add menu

---
+++ Lesson-06/03-Uber-Application.py
@@ -0,0 +1,19 @@
+def start():
+    while True:
+        start_option = int(input("---Start---\n"
+                                 "[1] Change service preference\n"
+                                 "[2] View history\n"
+                                 "[3] Book a ride\n"
+                                 "\n"
+                                 "Enter a number: "))
+        if start_option == 1:
+
+
+        elif start_option == 2:
+
+
+        elif start_option == 3:
+
+        else:
+
+start()
